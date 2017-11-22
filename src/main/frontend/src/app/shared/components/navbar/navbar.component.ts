4f6a0a9a823013001e6104517e74f52a807934e9
Jun Kawa <hubukinokaze@live.com>
2017-11-22 03:59:54 -0800

    + fixed github button

--- src/main/frontend/src/app/shared/components/navbar/navbar.component.ts
+++ src/main/frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+
+  public goToUrl() {
+    window.location.href = 'https://github.com/hubukinokaze/employee-dashboard';
+  }
 }
